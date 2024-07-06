917382d15663b2a515f93d9b636e41cf75defb6a
Thanh Vinh <vuthanhvinh438@gmail.gmailcom>
2024-07-06 22:47:30 +0700

    create log in and sign up page

--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -1,9 +1,22 @@
-// const { statusCode, message } = require('../constant/response');
-// const appError = require('../utils/appError');
-// const pool = require('../config/db.config');
+const { statusCode, message } = require('../constant/response');
+const appError = require('../utils/appError');
+const pool = require('../config/db.config');
 
-// const signUp = async (req, res, next) => {};
+const signUp = async (req, res, next) => {
+    try {
+    } catch (error) {
+        next(new appError(error));
+    }
+};
 
-// module.exports = {
-//     signUp,
-// };
+const logIn = async (req, res, next) => {
+    try {
+    } catch (error) {
+        next(new appError(error));
+    }
+};
+
+module.exports = {
+    signUp,
+    logIn,
+};
--- src/routes/clientRoute/index.js
+++ src/routes/clientRoute/index.js
@@ -2,10 +2,17 @@ const express = require('express');
 const axios = require('../../config/axios.config');
 const clientRoute = express.Router();
 
+// get home page
 clientRoute.route('/').get(async (req, res) => {
     const getData = await axios.get('/api/v1');
     console.log(getData.data);
     return res.render('home', { data: getData.data });
 });
+
+// get log in page
+clientRoute.route('/login').get((req, res) => res.render('logIn'));
+
+// get sign up page
+clientRoute.route('/signup').get((req, res) => res.render('signUp'));
 
 module.exports = clientRoute;
